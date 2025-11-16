7a9ab31b05f3c3ee84125c27f74112e01f7f2d21
mmkunlanat <stu6612732138@sskru.ac.th>
2025-11-16 15:23:09 +0700

    start

--- webnext/next.config.ts
+++ webnext/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone", // สำคัญสำหรับ deploy ขึ้น Vercel
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.vercel.app", // อนุญาตให้โหลดรูปจากโดเมนนี้
+      },
+    ],
+  },
 };
 
 export default nextConfig;
